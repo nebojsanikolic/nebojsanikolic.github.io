9a6ad62d279417d42bb5fe9e1f267a9b9f490458
Nebojša Nikolć <nikolic.nebojsa.va@gmail.com>
2021-04-20 23:11:18 +0200

    Gotovooo

---
+++ script.js
@@ -0,0 +1,33 @@
+AOS.init({
+    duration: 1200,
+  })
+
+  document.querySelectorAll('a[href^="#naslov"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+  
+
+/*Scroll to top when arrow up clicked BEGIN*/
+$(window).scroll(function() {
+  var height = $(window).scrollTop();
+  if (height > 100) {
+      $('#back2Top').fadeIn();
+  } else {
+      $('#back2Top').fadeOut();
+  }
+});
+$(document).ready(function() {
+  $("#back2Top").click(function(event) {
+      event.preventDefault();
+      $("html, body").animate({ scrollTop: 0 }, "slow");
+      return false;
+  });
+
+});
+/*Scroll to top when arrow up clicked END*/
